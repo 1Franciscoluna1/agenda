6bec13f064962ae1be3d3db2d13f707939aff074
1Franciscoluna1 <79699384+1Franciscoluna1@users.noreply.github.com>
2021-06-03 21:54:14 -0500

    Menu terminado

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+
+void mostrarmenu();
+
+
+struct Datos
+{
+	char nomre[25], numero[13];
+};
+
+
+int main() {
+
+	mostrarmenu();
+	system("pause");
+
+}
+
+void mostrarmenu() {
+	printf("\n\n\t\t\t Agenda bien mamalona");
+	printf("\n\t\tElija una opcion");
+	printf("\n\n\t\ta)\tAgregar contacto");
+	printf("\n\t\tb)\tModificar contacto");
+	printf("\n\t\tc)\tEliminar contacto");
+	printf("\n\t\td)\tbloquear contacto");
+	printf("\n\t\te)\tbuscar contacto");
+	printf("\n\t\tf)\tmostrar contactos");
+	printf("\n\t\tg)\tQuitar bloqueo a contacto");
+	printf("\n\t\th)\tSalir\n\n");
+
+
+}
