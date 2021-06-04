4a79c7b6526d1c6a91c4f5b708bb84eb7d5d1165
1Franciscoluna1 <79699384+1Franciscoluna1@users.noreply.github.com>
2021-06-03 21:57:59 -0500

    Se comento la parte del menu que no se utilizara

--- Source.cpp
+++ Source.cpp
@@ -21,12 +21,12 @@ void mostrarmenu() {
 	printf("\n\n\t\t\t Agenda bien mamalona");
 	printf("\n\t\tElija una opcion");
 	printf("\n\n\t\ta)\tAgregar contacto");
-	printf("\n\t\tb)\tModificar contacto");
-	printf("\n\t\tc)\tEliminar contacto");
-	printf("\n\t\td)\tbloquear contacto");
-	printf("\n\t\te)\tbuscar contacto");
-	printf("\n\t\tf)\tmostrar contactos");
-	printf("\n\t\tg)\tQuitar bloqueo a contacto");
+	//printf("\n\t\tb)\tModificar contacto");
+	//printf("\n\t\tc)\tEliminar contacto");
+	//printf("\n\t\td)\tbloquear contacto");
+	//printf("\n\t\te)\tbuscar contacto");
+	//printf("\n\t\tf)\tmostrar contactos");
+	//printf("\n\t\tg)\tQuitar bloqueo a contacto");
 	printf("\n\t\th)\tSalir\n\n");
 
 
